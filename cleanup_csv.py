fc40e05c066a40f6c421e54387037913c64c8f93
Louie Simpson <louie@louie.io>
2019-02-27 11:20:11 -0800

    Found populated FEDRAMP control xlsx

--- cleanup_csv.py
+++ cleanup_csv.py
@@ -1,16 +1,86 @@
 
 import json, csv, io, codecs, os, docx, re
+csv_dir = './csv'
+files = [
+    {'filename': 'FSCB_High.csv', 'Impact': 'High'},
+    {'filename': 'FSCB_Moderate.csv', 'Impact': 'Moderate'},
+    {'filename': 'FSCB_Low.csv', 'Impact': 'Low'}
+    ]
 
 
-doc = docx.Document("./fedramp.docx")
+def find_enhancement_by_control_id(id):
+    for control in controls['Controls']:
+        for e in controls['Controls'][control]["Enhancements"]:
+            if(e['ID'] == id):
+                return e
+    
+
+def find_control_by_id(id):
+    for control in controls['Controls']:
+        if control == id:
+            return control
 
 
-for para in doc.paragraphs:
-    if "AC-" in para.text:
-        print(re.search("^([^\s]+)", para.text))
+controls = {'Controls': {}}
+idx = 0
+for file in files:
+    with io.open(csv_dir + "/" + file['filename'], encoding='utf-8') as f:
+        reader = csv.DictReader( f, fieldnames = ('Count','SORT ID','Family','ID','Control Name','Control Description','Parameters','Further Guidance','FedRAMP Parameter','','',''))
+        
 
-#with open('fedramp.txt', 'wb') as f:
-#    json.dump(text, codecs.getwriter('utf-8')(f), ensure_ascii=False)
+        for row in reader:
+            foo = []
+
+
+            is_enhancement = False
+            #Id we are on top row skip to next
+            if row['ID'] == 'ID':
+                continue
+
+            # If there is a parantheses in the title it is an enhancement
+            if '(' in row['ID']:
+                foo = row['ID'].split(" ")
+                is_enhancement = True
+                tc = find_control_by_id(foo[0])
+            else:
+                tc = find_control_by_id(row['ID'])
+            #Set up a blank object
+            control = {}
+            
+            te = find_enhancement_by_control_id(row['ID'])
+            
+            #If there is an enancement we need to get the parent control
+            if is_enhancement:
+                if te:
+                    te['Impacts'].append(file['Impact'])
+                else: 
+                    enhancement = { 
+                        "ID": row["ID"], 
+                        "ControlText": row['Control Description'],
+                        "Impacts": [file['Impact']],
+                        "FedrampGuidance": row['Further Guidance']
+                        }
+                    if controls['Controls'][tc]:
+                        controls['Controls'][tc]["Enhancements"].append(enhancement)
+
+                is_enhancement = False
+            else: 
+                if tc:
+                    controls['Controls'][tc]['Impacts'].append(file['Impact'])
+                else:
+                    control = { 
+                        "ID": row["ID"], 
+                        "TITLE": row['Control Name'],
+                        "ControlText": row['Control Description'],
+                        "Impacts": [file['Impact']],
+                        "Enhancements": [],
+                        "FedrampGuidance": row['Further Guidance']
+                        }
+                    controls['Controls'][row["ID"]] = control
+
+#print(controls)
+with open('./output/fedramp_controls.json', 'wb') as f:
+   json.dump(controls, codecs.getwriter('utf-8')(f), ensure_ascii=False)
 
 # pathName = os.getcwd()
 # file_list = []
--- generate_json.sh
+++ generate_json.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-echo "Extracting Tables"
-python extract_table.py
+echo "Processing Controls"
+python cleanup_csv.py
 
-echo "Make csv dir and move files"
-rm -rf csv &&  mkdir csv && mv fedramp_*.csv csv/
-
-echo "Convert CSV"
-python convert.py
 
 
