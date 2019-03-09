f0f09f55df77eb27e1bf0e0173b13027d90df641
Louie Simpson <louie@louie.io>
2019-03-08 17:32:33 -0800

    Added some command line args

--- README.md
+++ README.md
@@ -3,9 +3,17 @@ You will need several python libraries.
 docx2csv
 requests
 
+PYTHON 3!!!!!!!!!!!!!!!
+
 if you don't have it
    - pip install docx2csv
    - pip install requests
 
-./generate_json.sh
+
+
+This will generate a smaller set of data so you won't have to wait so long for jekyll to build.
+./generate_json.sh -e dev
+
+This will generate the prod dataset
+./generate_json.sh -e prod
 
--- generate_json.sh
+++ generate_json.sh
@@ -1,11 +1,44 @@
 #!/bin/bash
 
-if [[ $1 == "-m" ]]; then
-    CREATE_MARKDOWN=1
-fi
-# \?) echo "generate_json [-m] optional m flag to generate markdown file";;
-# esac
-# done
+
+display_usage() {
+  echo
+  echo "Usage: $0"
+  echo
+  echo " -h, --help   Display usage instructions"
+  echo " -e, --env  prod,dev"
+  echo
+}
+
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+# Initialize our own variables:
+output_file=""
+verbose=0
+
+while getopts "h?me:" opt; do
+    case "$opt" in
+    h|\?)
+        display_usage
+        exit 0
+        ;;
+    m)  markdown=1
+        ;;
+    e)  environment=$OPTARG
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "${1:-}" = "--" ] && shift
+
+
+
+
+
+
 
 echo "Creating output dir"
 mkdir -p output/
@@ -13,11 +46,26 @@ mkdir -p output/
 echo "Processing Controls"
 python cleanup_csv.py
 
+#shorten the list a little bit
+if [[ $environment == "dev" ]]; then
+echo "Cutting records way back to avoid build times being really long"
 
-if [[ $CREATE_MARKDOWN == "1" ]]; then
+cat output/fedramp_controls.json |  jq 'limit(10;.[])' > output/controls.json
+echo "Created output/controls.json shortened dev version for use in explorer project"
+rm -rf output/fedramp_controls.json
+else
+echo "Created output/controls.json for prod use in explorer project"
+mv output/fedramp_controls.json output/controls.json
+fi
+
+if [[ $markdown == "1" ]]; then
     echo "Generating fedramp_controls.markdown"
     torsimany output/fedramp_controls.json
 fi
+
+
+
+
 
 
 
